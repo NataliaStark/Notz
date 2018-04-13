3303b5afe817861d1625487d01f8e8474696dc42
nata <natastark92@gmail.com>
2018-04-13 17:25:41 +0200

    second commit

---
+++ app/src/main/java/eu/fse/notz/MainActivity.java
@@ -0,0 +1,37 @@
+package eu.fse.notz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Amministratore on 13/04/2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] myDataset={"note1", "note2", "note3"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter
+        mAdapter = new NotesAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
---
+++ app/src/main/java/eu/fse/notz/NoteActivity.java
@@ -0,0 +1,10 @@
+package eu.fse.notz;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Amministratore on 13/04/2018.
+ */
+
+public class NoteActivity extends AppCompatActivity {
+}
